57184a11adea6773ae25a2adc3f578972cc5041f
feds201 <feds201@gmail.com>
2023-01-28 11:37:18 -0500

    Updated Constants

--- robot/template/src/main/java/frc/robot/Constants.java
+++ robot/template/src/main/java/frc/robot/Constants.java
@@ -3,35 +3,60 @@ package frc.robot;
 public class Constants {
   public static final class ModuleConstants {
     public static final int kSwerveFrontLeftDrive = 42;
-    public static final int SWERVE_FRONT_RIGHT_DRIVE = 22;
-    public static final int SWERVE_BACK_LEFT_DRIVE = 32;
-    public static final int SWERVE_BACK_RIGHT_DRIVE = 12;
+    public static final int kSwerveFrontRightDrive = 22;
+    public static final int kServeBackLeftDrive = 32;
+    public static final int kServeBackRightDrive = 12;
 
-    public static final int SWERVE_FRONT_LEFT_STEER = 41;
-    public static final int SWERVE_FRONT_RIGHT_STEER = 21;
-    public static final int SWERVE_BACK_LEFT_STEER = 31;
-    public static final int SWERVE_BACK_RIGHT_STEER = 11;
+    public static final int kSwerveFrontLeftSteer = 41;
+    public static final int kSwerveFrontRightSteer = 21;
+    public static final int kSwerveBackLeftSteer = 31;
+    public static final int kSwerveBackRightSteer = 11;
 
-    public static final double FRONT_LEFT_ENCODER_OFFSET = 0.6527862548828125;
-    public static final double FRONT_RIGHT_ENCODER_OFFSET = 0.759521484375;
-    public static final double BACK_LEFT_ENCODER_OFFSET = 0.01407623291015625;
-    public static final double BACK_RIGHT_ENCODER_OFFSET = 0.16002655029296875;
+    public static final double kFrontLeftEncoderOffset = 0.6527862548828125;
+    public static final double kFrontRightEncoderOffset = 0.759521484375;
+    public static final double kBackLeftEncoderOffset = 0.01407623291015625;
+    public static final double kBackRightEncoderOffset = 0.16002655029296875;
 
-    public static final int SWERVE_FRONT_LEFT_ENCODER = 4;
-    public static final int SWERVE_FRONT_RIGHT_ENCODER = 2;
-    public static final int SWERVE_BACK_LEFT_ENCODER = 3;
-    public static final int SWERVE_BACK_RIGHT_ENCODER = 1;
-    public static final int SWERVE_PIGEON = 0;
+    public static final int kSwerveFrontLeftEncoder = 4;
+    public static final int kSwerveFrontRightEncoder = 2;
+    public static final int kSwerveBackLeftEncoder = 3;
+    public static final int kSwerveBackRightEncoder = 1;
+    public static final int kSwervePigeon = 0;
   }
 
   public static final class PowerConstants {
-    public static final int PDP_CHANNEL = 1;
-    public static final int PCM_CHANNEL = 8;
+    public static final int kPDPChannel = 1;
+    public static final int kPCMChannel = 8;
   }
 
   public static final class OIConstants {
-    public static final double DEADZONE_THRESHOLD = 0.1;
+    public static final double kDeadzoneThreshold = 0.1;
     public static final int kDriveControllerPort = 0;
+  }
+
+  public static final class IntakeConstants {
+    public static final int kIntakeMotor1 = 0;
+    public static final int kIntakeMotor2 = 0;
+    public static final int kIntakeMotor3 = 0;
+    public static final int kIntakeMotor4 = 0;
+  }
+
+  public static final class ArmConstants {
+    public static final int kArmMotor1 = 0;
+    public static final int kArmMotor2 = 0;
+  }
+
+  public static final class TelescopeConstants {
+    public static final int kTelescopeMotor = 0;
+  }
+
+  public static final class OrientatorConstants {
+    public static final int kOrientatorMotor1 = 0;
+    public static final int kOrientatorMotor2 = 0;
+  }
+
+  public static final class ClawConstants {
+    public static final int kClawMotor = 0;
   }
 
 }
---
+++ robot/template/src/main/java/frc/robot/subsystems/OrientatorSubsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class OrientatorSubsystem extends SubsystemBase {
+    public OrientatorSubsystem(){
+        
+    }
+}
