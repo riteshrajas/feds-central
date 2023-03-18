1ded60ad9def2167f822715cb7ab58914547cdcb
Nihar Ogale <ogalenihar@gmail.com>
2023-03-18 15:20:59 -0400

    makes run Intake wheels

--- robot/template/src/main/java/frc/robot/commands/intake/RunIntakeWheels.java
+++ robot/template/src/main/java/frc/robot/commands/intake/RunIntakeWheels.java
@@ -2,19 +2,37 @@ package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj.Timer;
 
 public class RunIntakeWheels extends CommandBase {
     private final IntakeSubsystem m_intake;
+    private final Timer timer;
 
     public RunIntakeWheels(IntakeSubsystem intake) {
         this.m_intake = intake; 
+        timer = new Timer();
 
         addRequirements(m_intake);
     } 
 
+    @Override 
+    public void initialize(){
+        timer.reset();
+        timer.start();
+    }
+    
     @Override
     public void execute() {
         m_intake.runIntakeWheelsIn();
     }
 
+    @Override
+    public boolean isFinished(){
+        return timer.hasElapsed(2);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        m_intake.stopIntakeWheels();
+    }
 }
