28995cbc01edc8fd0a38c64adae993eff1052260
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2024-12-28 11:47:13 -0500

    Migrate next.config.js to next.config.mjs and refactor Nextra configuration

--- operations-manual/next.config.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const withNextra = await import('nextra').then(mod => mod.default);
-    module.exports = withNextra({
-        theme: 'nextra-theme-docs',
-        themeConfig: './theme.config.jsx'
-    });
-})();
