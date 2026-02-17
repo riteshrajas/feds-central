a73e9c6fe2d9d0846394c11e2ebd2ad8c43f0158
riteshrajas <riteshrajas.arulselvan@gmail.com>
2026-02-16 23:13:38 -0500

    Adds logging and simulation support
    Adds logging using AdvantageKit for data collection and analysis in real, simulation, and replay modes.
    
    Configures simulation environment with team number and alliance station ID.
    
    Logs command scheduling events for debugging and performance analysis.
    
    Co-authored-by: PhantomPorter <104171115+PhantomFool@users.noreply.github.com>

--- robot/2026-rebuilt/2026rebuilt/src/main/java/frc/robot/Robot.java
+++ robot/2026-rebuilt/2026rebuilt/src/main/java/frc/robot/Robot.java
@@ -4,12 +4,29 @@
   
 package frc.robot;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import com.google.flatbuffers.Constants;
+
+import edu.wpi.first.hal.AllianceStationID;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 //comment out the above line if you don't have a LedsSubsystem, and comment out the line in RobotContainer that creates the LedsSubsystem, and comment out the line in RobotContainer that sets the default command for the LedsSubsystem. You can also delete the LedsSubsystem class if you don't have it, but it's easier to just comment out those lines.
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
@@ -18,6 +35,55 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+
+      switch (RobotMap.getRobotMode()) {
+      case REAL:
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case SIM:
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case REPLAY:
+        String inPath = LogFileUtil.findReplayLog();
+        String outPath = LogFileUtil.addPathSuffix(inPath, "_sim");
+        Logger.setReplaySource(new WPILOGReader(inPath));
+        Logger.addDataReceiver(new WPILOGWriter(outPath));
+        break;
+    }
+
+    Logger.start();
+
+     // Silence joystick alerts
+    DriverStation.silenceJoystickConnectionWarning(true);
+
+    // Log active commands
+    Map<String, Integer> commandCounts = new HashMap<>();
+    BiConsumer<Command, Boolean> logCommandFunction =
+        (Command command, Boolean active) -> {
+          String name = command.getName();
+          int count = commandCounts.getOrDefault(name, 0) + (active ? 1 : -1);
+          commandCounts.put(name, count);
+          Logger.recordOutput(
+              "CommandsUnique/" + name + "_" + Integer.toHexString(command.hashCode()), active);
+          Logger.recordOutput("CommandsAll/" + name, count > 0);
+        };
+    CommandScheduler.getInstance()
+        .onCommandInitialize((Command command) -> logCommandFunction.accept(command, true));
+    CommandScheduler.getInstance()
+        .onCommandFinish((Command command) -> logCommandFunction.accept(command, false));
+    CommandScheduler.getInstance()
+        .onCommandInterrupt((Command command) -> logCommandFunction.accept(command, false));
+
+    // Configure Driver Station for sim
+    RoboRioSim.setTeamNumber(201);
+    if (RobotMap.getRobotMode() == RobotMap.robotState.SIM) {
+      DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
+      DriverStationSim.notifyNewData();
+    }
+
 
 
   }
--- robot/2026-rebuilt/2026rebuilt/src/main/java/frc/robot/RobotMap.java
+++ robot/2026-rebuilt/2026rebuilt/src/main/java/frc/robot/RobotMap.java
@@ -5,10 +5,18 @@ package frc.robot;
  * to a variable name. This provides flexibility changing wiring.
  */
 public final class RobotMap {
+
+    public enum robotState{
+        SIM,REAL,REPLAY;
+    }
     public static final class IntakeSubsystemConstants {
         public static final int kMotorID = 1;
         public static final int kLimit_switch_rID = 2;
         public static final int kLimit_switch_lID = 3; 
 
+    }
+
+    public static robotState getRobotMode() {
+        return Robot.isReal() ? robotState.REAL : robotState.SIM;
     }
 }
